4a7d8d61b33b0c709d2fbd46603b0998b85f190a
sekiro <1015004506@qq.com>
2021-03-26 13:48:33 +0800

    Add files via upload

---
+++ src/main/java/pers/quzhiyu/leetcode/RemoveDuplicatesFromSortedList.java
@@ -0,0 +1,27 @@
+package pers.quzhiyu.leetcode;
+
+import pers.quzhiyu.dataStructure.ListNode;
+
+public class RemoveDuplicatesFromSortedList {
+    public ListNode deleteDuplicates(ListNode head) {
+        ListNode cur = head;
+        while (cur != null) {
+            while (cur.next != null && cur.val == cur.next.val) {
+                cur.next = cur.next.next;
+            }
+            cur = cur.next;
+        }
+        return head;
+    }
+
+    public static void main(String[] args) {
+        ListNode listNode = new ListNode(1);
+        listNode.next = new ListNode(2);
+        listNode.next.next = new ListNode(3);
+        listNode.next.next.next = new ListNode(3);
+        listNode.next.next.next.next = new ListNode(4);
+        listNode.next.next.next.next.next = new ListNode(5);
+        listNode.next.next.next.next.next.next = new ListNode(5);
+        new RemoveDuplicatesFromSortedList().deleteDuplicates(listNode);
+    }
+}
