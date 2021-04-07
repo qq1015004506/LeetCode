c4a385d7d3acd155a106fed27c2823c1e8137bdb
qzy <1015004506@qq.com>
2021-04-07 09:39:42 +0800

    20210407

---
+++ src/main/java/pers/quzhiyu/leetcode/SearchInRotatedSortedArrayII.java
@@ -0,0 +1,12 @@
+package pers.quzhiyu.leetcode;
+
+public class SearchInRotatedSortedArrayII {
+    public boolean search(int[] nums, int target) {
+        for (int i = 0; i < nums.length; i++) {
+            if(nums[i] == target) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
