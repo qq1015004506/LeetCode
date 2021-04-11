d31a78ab51ca8109f9ed7511676ea53c18872649
qzy <1015004506@qq.com>
2021-04-11 16:30:23 +0800

    20210411

--- src/main/java/pers/quzhiyu/weekly/MinimumSidewayJumps.java
+++ src/main/java/pers/quzhiyu/weekly/MinimumSidewayJumps.java
@@ -3,32 +3,28 @@ package pers.quzhiyu.weekly;
 import java.util.Arrays;
 
 public class MinimumSidewayJumps {
-    public int minSideJumps(int[] ob) {
-        int n = ob.length;
-        int[] prev = new int[3];
-        int[] next = new int[3];
-        int inf = (int)1e9;
-        Arrays.fill(prev, inf);
-        prev[1] = 0;
-        for(int i = 0; i < n; i++){
-            int o = ob[i];
-            Arrays.fill(next, inf);
-            for(int j = 0; j < 3; j++){
-                for(int k = 0; k < 3; k++){
-                    if(o - 1 == k || o - 1 == j){
-                        continue;
-                    }
-                    next[k] = Math.min(next[k], prev[j] + (j != k ? 1 : 0));
-                }
-            }
-            int[] tmp = prev;
-            prev = next;
-            next = tmp;
+    public int minSideJumps(int[] obstacles) {
+        int n = obstacles.length;
+        int[] dp = new int[3];
+        dp[0] = dp[2] = 1;
+        for (int i = 1; i < n; i++) {
+            int obs = obstacles[i];
+            //初始化dp，分两步
+            int pre0 = dp[0];
+            int pre1 = dp[1];
+            int pre2 = dp[2];
+            //1.最大值填充
+            Arrays.fill(dp, Integer.MAX_VALUE - 10000);
+            //2.实际障碍物情况：如果 j 位置无障碍物，先更新为刚刚保存的前一位置pre的次数
+            if (obs != 1) dp[0] = pre0;
+            if (obs != 2) dp[1] = pre1;
+            if (obs != 3) dp[2] = pre2;
+            //比较从非 j 的位置跳过来，是否次数更小
+            if (obs != 1) dp[0] = Math.min(dp[0], Math.min(dp[1], dp[2]) + 1);
+            if (obs != 2) dp[1] = Math.min(dp[1], Math.min(dp[0], dp[2]) + 1);
+            if (obs != 3) dp[2] = Math.min(dp[2], Math.min(dp[0], dp[1]) + 1);
         }
-        int ans = Arrays.stream(prev).min().orElse(-1);
-        return ans;
-
-
+        return Arrays.stream(dp).min().orElse(-1);
     }
 
     public static void main(String[] args) {
