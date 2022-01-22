5d484556f7d09f1989f3514f680974472ff483a1
qzy <1015004506@qq.com>
2022-01-22 17:41:43 +0800

    20220122

--- src/main/java/pers/quzhiyu/leetcode/AddTwoNumbers.java
+++ src/main/java/pers/quzhiyu/leetcode/AddTwoNumbers.java
@@ -6,32 +6,32 @@ import java.util.ArrayDeque;
 import java.util.Deque;
 
 public class AddTwoNumbers {
-    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-        ListNode head = null, tail = null;
-        int carry = 0;
-        while (l1 != null || l2 != null) {
-            int n1 = l1 != null ? l1.val : 0;
-            int n2 = l2 != null ? l2.val : 0;
-            int sum = n1 + n2 + carry;
-            if (head == null) {
-                head = tail = new ListNode(sum % 10);
-            } else {
-                tail.next = new ListNode(sum % 10);
-                tail = tail.next;
+        public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+            ListNode head = null, tail = null;
+            int carry = 0;
+            while (l1 != null || l2 != null) {
+                int n1 = l1 != null ? l1.val : 0;
+                int n2 = l2 != null ? l2.val : 0;
+                int sum = n1 + n2 + carry;
+                if (head == null) {
+                    head = tail = new ListNode(sum % 10);
+                } else {
+                    tail.next = new ListNode(sum % 10);
+                    tail = tail.next;
+                }
+                carry = sum / 10;
+                if (l1 != null) {
+                    l1 = l1.next;
+                }
+                if (l2 != null) {
+                    l2 = l2.next;
+                }
             }
-            carry = sum / 10;
-            if (l1 != null) {
-                l1 = l1.next;
+            if (carry > 0) {
+                tail.next = new ListNode(carry);
             }
-            if (l2 != null) {
-                l2 = l2.next;
-            }
+            return head;
         }
-        if (carry > 0) {
-            tail.next = new ListNode(carry);
-        }
-        return head;
-    }
 
     public static void main(String[] args) {
         ListNode listNode1 = new ListNode(2);
---
+++ src/main/java/pers/quzhiyu/leetcode/RemovePalindromeSub.java
@@ -0,0 +1,13 @@
+package pers.quzhiyu.leetcode;
+
+public class RemovePalindromeSub {
+    public int removePalindromeSub(String s) {
+        int n = s.length();
+        for (int i = 0; i < n / 2; ++i) {
+            if (s.charAt(i) != s.charAt(n - 1 - i)) {
+                return 2;
+            }
+        }
+        return 1;
+    }
+}
